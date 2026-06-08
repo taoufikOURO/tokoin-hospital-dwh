be8284d609ca3d7b831f0f52a6b4851c5a7e3d34
Taoufik OURO-BANG'NA <ourotaoufik1@gmail.com>
2026-06-08 10:58:50 +0000

    Uncomment data loading, transformation, and patient dossier export functions in main script

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from processing.mongo_export import export_dossiers_patients
 from processing.mongo_queries import run_mongo_queries
 
 if __name__ == "__main__":
-    #load_all()
-    #transform_all()
-    #export_dossiers_patients()
+    load_all()
+    transform_all()
+    export_dossiers_patients()
     run_mongo_queries()
